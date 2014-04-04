cbe8458a00c60a9c386f12936691eca7c5dcfe85
JaimeLarivedroite <cm@blog-rivedroite.fr>
2014-04-04 16:21:58 +0200

    Refactoring des features RSpec.
    Step de refactoring des features RSpec avec scenarios plus explicite et meilleure utilisation
    des matchers Capybara.
    Le prochain step de refactoring intégrera l'internationalisation et l'automatisation avec Guard.

--- spec/features/visitor_logs_in_spec.rb
+++ spec/features/visitor_logs_in_spec.rb
@@ -2,39 +2,42 @@ require 'spec_helper'
 
 feature 'Visitor logs in' do
   background do
-    create(:user, :email => 'valid@example.com', :password => 'password', :nickname => 'valid.user')
+    @user = create(:user)
   end
 
-  scenario 'with valid email and password' do
-    login_with 'valid@example.com', 'password'
+  scenario 'successfully with valid email and password' do
+    login_with_email @user
 
-    expect(page).to have_content('Log out')
+    expect(page).to have_content("Logged in")
     expect(current_path).to eq(root_path)
   end
 
-  scenario 'with valid nickname and password' do
-    login_with_nickname 'valid.user', 'password'
+  scenario 'successfully with valid nickname and password' do
+    login_with_nickname  @user
 
-    expect(page).to have_content('Log out')
+    expect(page).to have_content("Logged in")
     expect(current_path).to eq(root_path)
   end
 
-  scenario 'with invalid email' do
-    login_with 'invalid@example.com', 'password'
+  scenario 'unsuccessfully with invalid email' do
+    user = build(:user, email: "nonexisting@hlm.fr")
+    login_with_email user
 
     expect(page).to have_content('Log in')
     expect(current_path).to eq(sessions_path)
   end
 
-  scenario 'with invalid nickname' do
-    login_with 'invalid.user', 'password'
+  scenario 'unsuccessfully with invalid nickname' do
+    user = build(:user, nickname: "nonexisting.hlm")
+    login_with_nickname user
 
     expect(page).to have_content('Log in')
     expect(current_path).to eq(sessions_path)
   end
 
-  scenario 'with invalid password' do
-    login_with 'valid@example.com', 'drowssap'
+  scenario 'unsuccessfully with invalid password' do
+    user = build(:user, password: "drowssap")
+    login_with_email user
 
     expect(page).to have_content('Log in')
     expect(current_path).to eq(sessions_path)
@@ -43,12 +46,12 @@ feature 'Visitor logs in' do
   scenario 'and can sign up on the same page' do
     visit login_path
 
-    expect(page).to have_content('Sign up')
+    expect(page).to have_content("Sign up")
   end
 
   scenario 'and can reset password on the same page' do
     visit login_path
 
-    expect(page).to have_content('Reset password')
+    expect(page).to have_content("Reset password")
   end
 end
--- spec/features/visitor_signs_up_spec.rb
+++ spec/features/visitor_signs_up_spec.rb
@@ -1,31 +1,53 @@
 require 'spec_helper'
 
 feature 'Visitor signs up' do
-  scenario 'with valid email, nickname and password' do
-    sign_up_with 'valid@example.com', 'valid.user', 'password'
+  scenario 'create account with valid email, nickname and password' do
+    user = build(:user)
+    sign_up_with user
 
-    expect(page).to have_content('Log out')
+    expect(page).to have_content("Account successfully created")
     expect(current_path).to eq(root_path)
   end
 
-  scenario 'with invalid email' do
-    sign_up_with 'invalid_email', 'valid.user', 'password'
+  scenario 'does not create account with invalid email' do
+    user = build(:user, email: "invalid_email.fr")
+    sign_up_with user
 
-    expect(page).to have_content('Sign up')
+    expect(page).to have_content("wrong email address")
     expect(current_path).to eq(users_path)
   end
 
-  scenario 'with nil nickname' do
-    sign_up_with 'valid@example.com', nil, 'pass'
+  scenario 'does not create account with nil nickname' do
+    user = build(:user, nickname: nil)
+    sign_up_with user
 
-    expect(page).to have_content('Sign up')
+    expect(page).to have_content("Nickname can't be blank")
     expect(current_path).to eq(users_path)
   end
 
-  scenario 'with invalid password' do
-    sign_up_with 'valid@example.com', 'valid.user', 'pass'
+  scenario 'does not create account with invalid password' do
+    user = build(:user, password: "short")
+    sign_up_with user
 
-    expect(page).to have_content('Sign up')
+    expect(page).to have_content("Password is too short")
+    expect(current_path).to eq(users_path)
+  end
+
+  scenario 'does not create account with duplicated email' do
+    create(:user)
+    user = build(:user, nickname: "another.nickname")
+
+    sign_up_with user
+    expect(page).to have_content("Email is already taken")
+    expect(current_path).to eq(users_path)
+  end
+
+  scenario 'does not create account with duplicated nickname' do
+    create(:user)
+    user = build(:user, email: "another@hlm.fr")
+
+    sign_up_with user
+    expect(page).to have_content("Nickname is already taken")
     expect(current_path).to eq(users_path)
   end
 end
